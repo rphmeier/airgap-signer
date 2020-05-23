848b466e920efbab5da2a0dbcd1929f3026ae1ac
Robert Habermeier <rphmeier@gmail.com>
2020-05-23 16:34:43 -0400

    skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState, Dispatch, SetStateAction } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { InjectedExtension, InjectedAccount } from '@polkadot/extension-inject/types';
+import { web3Accounts, web3Enable, web3FromAddress } from '@polkadot/extension-dapp';
+
+const APP_NAME: string = "Airgap-Signer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [injected, setInjected] = useState([] as InjectedExtension[]);
+
+  useEffect(() => { web3Enable(APP_NAME).then(i => setInjected(i))}, [injected]);
+
+  if (injected.length == 0) {
+    return <p>No injected signers, initializing?</p>
+  } else {
+    return <InitializedApp />
+  }
+}
+
+// The signing process takes on these phases:
+// 1. Choose account / network
+// 2. Choose signing data (QR / Hex)
+// 3. Sign!
+// 4. View signature as hex or QR code.
+
+function InitializedApp() {
+  return <p>Wahoo!</p>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { web3Accounts, web3Enable, web3FromAddress } from '@polkadot/extension-dapp';
+
 
 ReactDOM.render(
   <React.StrictMode>
